6e29bb3a8a7fc172a44f993066efa07665f28d59
mtsguerra7 <mtsguerra9@gmail.com>
2025-05-13 15:16:01 +0100

    Creating structure

---
+++ game_structure/style.py
@@ -0,0 +1,25 @@
+# COLORS
+BOARD_COLOR = (63, 28, 215)  # Blue
+BACKGROUND_COLOR = (250, 250, 250)  # White
+GRAY = (170, 170, 170)  # Gray
+PLAYER_COLOR = (245, 0, 0)  # Red
+SECOND_PLAYER_COLOR = (255, 215, 0)  # Yellow
+BLACK = (0, 0, 0)  # Black
+
+PIECES_COLORS = [BACKGROUND_COLOR, PLAYER_COLOR, SECOND_PLAYER_COLOR]
+FIRST_PLAYER_PIECE = 1 # Value for the first player
+SECOND_PLAYER_PIECE = 2 # Value for the second player
+
+# Constants for the data matrix
+ROWS = 6
+COLUMNS = 7
+
+# Constants for the interface (board)
+SQUARESIZE = 110  # Size of the square
+RADIUS_PIECE = 50  # Radius of the piece
+
+# Constants for the screen
+WIDTH = (3 + COLUMNS) * SQUARESIZE  # width of the screen = board + 1 and a half empty column on the left and right
+HEIGHT = (ROWS + 2) * SQUARESIZE  # height of the screen = board + 1 empty row on the top and bottom
+
+
