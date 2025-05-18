941146366b454a9ca29fb93d71c08aa2611e88d7
Matheus Goncalves Guerra <mtsguerra9@gmail.com>
2025-05-18 18:29:03 +0100

    Update style.py

--- game_structure/style.py
+++ game_structure/style.py
@@ -1,30 +1,29 @@
-# COLORS
-RED = (229, 37, 33)  # Mario red
-GREEN = (67, 176, 71)  # Mario green
-BLUE = (4, 156, 216)  # Mario blue
-YELLOW = (251, 208, 0)  # Mario yellow
-BLACK = (0, 0, 0)  # Black
-GRAY = (170, 170, 170)  # Gray
-WHITE = (255, 255, 255)  # White
+# CORES
+RED = (229, 37, 33)  # Vermelho Mario
+GREEN = (67, 176, 71)  # Verde Mario
+BLUE = (4, 156, 216)  # Azul Mario
+YELLOW = (251, 208, 0)  # Amarelo Mario
+BLACK = (0, 0, 0)  # Preto
+GRAY = (170, 170, 170)  # Cinza
+WHITE = (255, 255, 255)  # Branco
 
-
-BOARD_COLOR = BLUE  # Blue
-BACKGROUND_COLOR = (155, 225, 255)  # Baby blue
-PLAYER_COLOR = (245, 0, 0)  # Red
-SECOND_PLAYER_COLOR = (255, 215, 0)  # Yellow
+BOARD_COLOR = (0,50,245) # Azul tabuleiro
+BACKGROUND_COLOR = (155, 225, 255)  # Azul bebê
+PLAYER_COLOR = (245, 0, 0)  # Vermelho peça
+SECOND_PLAYER_COLOR = (255, 215, 0)  # Amarelo peça
 
 PIECES_COLORS = [WHITE, RED, YELLOW]
-FIRST_PLAYER_PIECE = 1 # Value for the first player
-SECOND_PLAYER_PIECE = 2 # Value for the second player
+FIRST_PLAYER_PIECE = 1  # Valor para o primeiro jogador
+SECOND_PLAYER_PIECE = 2  # Valor para o segundo jogador
 
-# Constants for the data matrix
+# Constantes da matriz de dados
 ROWS = 6
 COLUMNS = 7
 
-# Constants for the interface (board)
-SQUARE_SIZE = 100  # Size of the square
-RADIUS_PIECE = 45  # Radius of the piece
+# Constantes do tabuleiro
+SQUARE_SIZE = 100
+RADIUS_PIECE = 45 
 
-# Constants for the screen
-WIDTH = (4 + COLUMNS) * SQUARE_SIZE  # width of the screen = board + 1 and a half empty column on the left and right
-HEIGHT = (2 + ROWS) * SQUARE_SIZE  # height of the screen = board + 1 empty row on the top and bottom
+# Constantes da tela do jogo
+WIDTH = (4 + COLUMNS) * SQUARE_SIZE  # largura da tela = tabuleiro + 2 colunas vazias na direita e esquerda
+HEIGHT = (2 + ROWS) * SQUARE_SIZE  # altura da tela = tabuleiro + 1 linha vazia em cima e embaixo
